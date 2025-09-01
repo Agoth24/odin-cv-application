a3c46fdc10084c0d389822db946a14108a6c83e0
Agoth Arop <agoth.a3@gmail.com>
2025-09-01 10:31:16 -0600

    progressed on formatted CV structure and styling

--- src/components/FormattedCV.jsx
+++ src/components/FormattedCV.jsx
@@ -1,10 +1,18 @@
-import "../index.css";
+import "../styles/formattedCV.css";
 
 export default function FormattedCV({ onEdit, data }) {
-//   function handleClick(e) {
-//     e.preventDefault();
-//     onEdit();
-//   }
+
+function formatDate(dateStr) {
+    const date = new Date(dateStr);
+
+    return date.toLocaleString("en-US", {month: "short", year: "numeric"})
+}
+console.log(formatDate("2018-08-25"))
+
+  function handleClick(e) {
+    e.preventDefault();
+    onEdit();
+  }
 
   /* 
 formData properties
@@ -27,13 +35,31 @@ formData properties
   */
 
   return (
-    <section>
-      <header className="header">
-        <h1>{"John Smith"}</h1>
-      <div className="contact">
-        <span>{"john123@email.ca"}</span>
-      </div>
+    <article>
+      <header className="formatted-header">
+        <h1>{"Michael Williams"}</h1>
+        <div className="contact">
+          <span>{"mike123@email.ca"}</span>
+          <span>{"(416)-222-4109"}</span>
+        </div>
       </header>
-    </section>
+      <section className="infoSection">
+        <h2>Educational Experience</h2>
+        <ul>
+            <li className="info-group">
+                <div className="info-text">
+                    <h3>{"University of Waterloo"}</h3>
+                    <p>{"Bachelor of Science"}</p>
+                </div>
+                <div className="info-dates">
+                        {"Sep 2018 - April 2012"}
+                </div>
+            </li>
+        </ul>
+      </section>
+      <section className="infoSection">
+        <h2>Practical Work Experience</h2>
+      </section>
+    </article>
   );
 }
