e40e864ac96aa85cf5f26a8e0cf20620697c036c
Agoth Arop <agoth.a3@gmail.com>
2025-08-31 20:33:37 -0600

    Fixed the practical experience section

--- src/components/PracticalExp.jsx
+++ src/components/PracticalExp.jsx
@@ -1,14 +1,21 @@
-import '../App.css'
-import FormGroup from './FormGroup'
+import "../App.css";
+import FormGroup from "./FormGroup";
 
 export default function PracticalExp() {
-
-    return (
-        <section>
-            <h2>Practical Work Experience</h2>
-            <form>
-
-            </form>
-        </section>
-    )
+  return (
+    <section>
+      <h2>Practical Work Experience</h2>
+      <form>
+        <FormGroup id="companyName" labelText="Company:" type="text" />
+        <FormGroup id="positionTitle" labelText="Position Title:" type="text" />
+        <FormGroup
+          id="jobResp"
+          labelText="Main Responsibilities"
+          type="textarea"
+        />
+        <FormGroup id="fromDate" labelText="From:" type="date" />
+        <FormGroup id="toDate" labelText="To:" type="date" />
+      </form>
+    </section>
+  );
 }
